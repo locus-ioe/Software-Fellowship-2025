f2d7097afaaeb8a3824cb9f46c0d31a5fb90ec52
SusheelThapa <susheelthapa90@gmail.com>
2024-06-29 16:18:35 +0545

    feat initialize react project with vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="bg-black h-screen text-4xl w-screen text-white flex justify-center items-center">
+      Hello World
+    </div>
+  );
+};
+
+export default App;
