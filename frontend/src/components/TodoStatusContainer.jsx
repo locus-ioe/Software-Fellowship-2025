fa08f126f9295a02d1a517794fde637a7cdc4ba8
SusheelThapa <susheelthapa90@gmail.com>
2024-07-01 09:06:20 +0545

    feat: delete todo confirmation

--- frontend/src/components/TodoStatusContainer.jsx
+++ frontend/src/components/TodoStatusContainer.jsx
@@ -4,9 +4,9 @@ import { FaRegCircleCheck } from "react-icons/fa6";
 import { GrEdit } from "react-icons/gr";
 import { RiDeleteBinLine } from "react-icons/ri";
 
-const TodoStatusContainer = ({ title, todos, icon }) => {
+const TodoStatusContainer = ({ title, todos, icon, onDelete }) => {
   return (
-    <div className="shadow-2xl rounded-[5rem] bg-white px-12 py-10  h-fit">
+    <div className="shadow-2xl rounded-[5rem] bg-white px-12 py-10 w-1/2 h-fit">
       <div className="flex space-x-2 my-3 mb-6">
         <div className="flex justify-center items-center">{icon}</div>
         <div className="flex justify-center items-center text-xl font-semibold tracking-wide">
@@ -14,10 +14,10 @@ const TodoStatusContainer = ({ title, todos, icon }) => {
         </div>
       </div>
       <div className="flex flex-col space-y-10">
-        {todos.map(({ title, status, description }) => {
+        {todos.map(({ id, title, status, description }) => {
           return (
             <div
-              key={`${title}${status}`}
+              key={id}
               className={`${
                 status == "ongoing" ? "bg-[#FFAA041A]" : "bg-[#E6FAEA]"
               } rounded-3xl p-4 px-6`}
@@ -26,7 +26,10 @@ const TodoStatusContainer = ({ title, todos, icon }) => {
               <div className="text-sm text-gray-800 mb-6">{description}</div>
               <div className="text-[#B3B7EE] text-2xl flex space-x-6 justify-end">
                 <GrEdit className="cursor-pointer hover:text-blue-500" />
-                <RiDeleteBinLine className="cursor-pointer hover:text-red-500" />
+                <RiDeleteBinLine
+                  className="cursor-pointer hover:text-red-500"
+                  onClick={() => onDelete(id)}
+                />
                 <FaRegCircleCheck className="cursor-pointer hover:text-green-500" />
               </div>
             </div>
@@ -41,5 +44,6 @@ TodoStatusContainer.propTypes = {
   title: PropTypes.string,
   todos: PropTypes.array,
   icon: PropTypes.element,
+  onDelete: PropTypes.func,
 };
 export default TodoStatusContainer;
--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -9,14 +9,38 @@ import { IoIosAddCircle } from "react-icons/io";
 import { todosList } from "../assets/json/todos.json";
 
 const Todos = () => {
-  const [todos] = useState(todosList);
+  const [todos, setTodos] = useState(todosList);
+  const [deleteTodoID, setDeleteTodoID] = useState(null);
+  const [deleteTaskConfirmation, setDeleteTaskConfirmation] = useState(false);
 
   const filterTodos = (status) => {
     return todos.filter((todo) => todo.status == status);
   };
 
+  const handleDelete = () => {
+    /*
+    Here,we are just updating in the frontend side
+    In integration side we update in backend side too by making api call
+    */
+    const updatedTodoList = todos.filter((todo) => {
+      if (deleteTodoID == todo.id) {
+        todo.status = "deleted";
+      }
+      return todo;
+    });
+
+    setTodos(updatedTodoList);
+    setDeleteTodoID(null);
+    setDeleteTaskConfirmation(false);
+  };
+
+  const showDeleteConfirmation = (id) => {
+    setDeleteTodoID(id);
+    setDeleteTaskConfirmation(true);
+  };
+
   return (
-    <div>
+    <>
       <div id="title" className="text-stone-500 text-4xl font-black my-12">
         Organize your <span className="text-black">To-Dos</span>
       </div>
@@ -30,6 +54,7 @@ const Todos = () => {
               size={30}
             />
           }
+          onDelete={showDeleteConfirmation}
         />
         <TodoStatusContainer
           title="Completed"
@@ -40,6 +65,7 @@ const Todos = () => {
               size={30}
             />
           }
+          onDelete={showDeleteConfirmation}
         />
       </div>
       <div className="flex justify-center items-center mt-16">
@@ -48,7 +74,36 @@ const Todos = () => {
           <IoIosAddCircle className="inline cursor-pointer" size={30} />
         </div>
       </div>
-    </div>
+      {/* Delete Task Confirmation Popup Element */}
+      {deleteTaskConfirmation && (
+        <div className="fixed inset-0 z-50 ">
+          <div className="bg-[#00000088] w-screen h-screen flex justify-center items-center ">
+            <div className=" bg-white px-16 py-12 rounded-[45px] flex flex-col space-y-12 justify-center items-center">
+              <div className="text-4xl font-semibold tracking-wide">
+                <span className="text-stone-500">Delete</span> Task?
+              </div>
+              <div className="flex justify-center items-center gap-8 text-white text-xl tracking-wide">
+                <button
+                  className="rounded-3xl bg-[#578EFB] px-7 py-3 shadow-md hover:bg-[#2869eb]"
+                  onClick={handleDelete}
+                >
+                  Yes
+                </button>
+                <button
+                  className="rounded-3xl bg-gray-400 px-7 py-3 shadow-md hover:bg-gray-500"
+                  onClick={() => {
+                    setDeleteTodoID(null);
+                    setDeleteTaskConfirmation(false);
+                  }}
+                >
+                  No
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
