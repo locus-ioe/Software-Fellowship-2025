d8fa74b28a417ed84574bae772d7f57495044143
PRASUN-SITAULA <psitaula209@gmail.com>
2024-07-06 17:21:34 +0545

    Mongo setup

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,13 @@ require('dotenv').config()
 const app = require('./app')
 
 const port = process.env.PORT || 3000
+const DB = process.env.MONGO_URL
 
 console.log(port)
+
+// connect mongoose to mongodb
+mongoose.connect(DB, {
+}).then(() => console.log("database connection successful"))
 
 // run the server
 app.listen(port, () => {
