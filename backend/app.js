7198e95efde010d284140b1574ae98c7516e9d72
Surav Shrestha <surav.shrestha@outlook.com>
2024-07-11 20:10:24 +0545

    db-api: add api routes

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,11 @@ app.get("/api/todos", async (req, res) => {
 app.get("/api/todos/:id", async (req, res) => {
   try {
     const todo = await Todo.findById(req.params.id);
-    if (!todo) return res.status(404).json({ message: "Todo not found" });
+
+    if (!todo) {
+      return res.status(404).json({ message: "Todo not found" });
+    }
+
     res.json(todo);
   } catch (err) {
     res.status(500).json({ message: err.message });
@@ -49,8 +53,11 @@ app.patch("/api/todos/:id", async (req, res) => {
       new: true,
       runValidators: true,
     });
-    if (!updatedTodo)
+
+    if (!updatedTodo) {
       return res.status(404).json({ message: "Todo not found" });
+    }
+
     res.json(updatedTodo);
   } catch (err) {
     res.status(400).json({ message: err.message });
@@ -61,8 +68,11 @@ app.patch("/api/todos/:id", async (req, res) => {
 app.delete("/api/todos/:id", async (req, res) => {
   try {
     const deletedTodo = await Todo.findByIdAndDelete(req.params.id);
-    if (!deletedTodo)
+
+    if (!deletedTodo) {
       return res.status(404).json({ message: "Todo not found" });
+    }
+
     res.json({ message: "Todo deleted" });
   } catch (err) {
     res.status(500).json({ message: err.message });
