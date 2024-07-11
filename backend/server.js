3d5358f2af7e718c7374f2ae43d08d3b60f17023
Surav Shrestha <surav.shrestha@outlook.com>
2024-07-11 19:48:37 +0545

    db-api: configure mongoose

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,18 @@ const app = require("./app");
 
 const port = process.env.PORT || 3000;
 
-console.log(port);
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("error connecting to MongoDB:", error.message);
+  });
 
 // run the server
 app.listen(port, () => {
-  console.log("server is listening at port ", port);
+  console.log("server is listening at port", port);
 });
