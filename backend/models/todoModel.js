8b341c5562394ee2d20177e7ccefccb82b39dd7f
Surav Shrestha <surav.shrestha@outlook.com>
2024-07-11 20:07:01 +0545

    db-api: _id -> id, remove __v while sending response

--- backend/models/todoModel.js
+++ backend/models/todoModel.js
@@ -14,5 +14,16 @@ const todoModelSchema = new mongoose.Schema({
   },
 });
 
+// .toJSON method is called everytime res.json is called
+// convert _id to id and remove _id and __v from the object
+todoModelSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
 const Todo = mongoose.model("Todo", todoModelSchema);
 module.exports = Todo;
