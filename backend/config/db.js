bc37263361de79b240ee8043f2a7744edf164a42
Surav Shrestha <surav.shrestha@outlook.com>
2024-07-13 09:42:10 +0545

    fix: remove useNewUrlParser
    (node:9989) [MONGODB DRIVER] Warning: useNewUrlParser is a deprecated option: useNewUrlParser has no effect since Node.js Driver version 4.0.0 and will be removed in the next major version

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-    });
+    await mongoose.connect(process.env.MONGODB_URI);
 
     console.log("Connected to MongoDB");
   } catch (error) {
